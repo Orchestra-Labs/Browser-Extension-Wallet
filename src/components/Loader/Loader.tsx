3a75f01e1e6b9a480a7e8137530807b6e56292fd
dblackstone1 <dblackstone@orchestralabs.org>
2024-10-25 22:46:01 -0400

    fix: prevent accidental triggers of refresh

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,7 +1,8 @@
 import { Spinner } from '@/assets/icons';
+import { cn } from '@/helpers';
 
-export const Loader = () => (
+export const Loader = ({ isSpinning = true }: { isSpinning?: boolean }) => (
   <div className="bg-background-dark-grey w-full h-full flex items-center justify-center">
-    <Spinner className="w-12 h-12 animate-spin fill-blue" />
+    <Spinner className={cn(`w-12 h-12 ${isSpinning ? 'animate-spin' : ''} fill-blue`)} />
   </div>
 );
--- src/components/TileScroller/TileScroller.tsx
+++ src/components/TileScroller/TileScroller.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { ScrollArea } from '@/ui-kit';
-import { AssetTiles } from './AssetTiles';
-import { ValidatorTiles } from './ValidatorTiles';
 import { Asset, CombinedStakingInfo } from '@/types';
 import { useDrag } from '@use-gesture/react';
 import { animated, useSpring } from 'react-spring';
+import { Loader } from '../Loader';
+import { ValidatorTiles } from '../TileScroller/ValidatorTiles';
+import { AssetTiles } from '../TileScroller/AssetTiles';
 
 interface TileScrollerProps {
   activeIndex: number;
@@ -24,58 +25,88 @@ export const TileScroller: React.FC<TileScrollerProps> = ({
   isReceiveDialog = false,
 }) => {
   const [isRefreshing, setIsRefreshing] = useState(false);
+  const [triggerRefresh, setTriggerRefresh] = useState(false);
   const [isMouseDown, setIsMouseDown] = useState(false);
   const [dragStarted, setDragStarted] = useState(false);
   const viewportRef = useRef<HTMLDivElement>(null);
 
-  // Using react-spring to control bounce effect
-  const [{ y }, api] = useSpring(() => ({ y: 0 }));
+  const TOP_OVERSCROLL_LIMIT = 52;
+  const OVERSCROLL_ACTIVATION_THRESHOLD = TOP_OVERSCROLL_LIMIT * 0.8;
+  const BOTTOM_OVERSCROLL_PERCENTAGE = 0.15;
+  const MAX_REFRESH_VELOCITY = 0.5;
+
+  const [{ y, loaderOpacity }, api] = useSpring(() => ({ y: 0, loaderOpacity: 0 }));
 
   const handleRefresh = async () => {
     console.log('Refreshing...');
     setIsRefreshing(true);
+    api.start({ loaderOpacity: 1 });
     await new Promise(resolve => setTimeout(resolve, 1000));
     setIsRefreshing(false);
+    api.start({ y: 0, loaderOpacity: 0 });
   };
 
-  const bind = useDrag(
-    ({ movement: [, my], memo = viewportRef.current?.scrollTop || 0, event, dragging, last }) => {
-      console.log(
-        `Dragging: ${dragging}, Last: ${last}, MouseY: ${my}, Memo: ${memo}, DragStarted: ${dragStarted}, isMouseDown: ${isMouseDown}`,
-      );
+  useEffect(() => {
+    if (triggerRefresh) {
+      handleRefresh();
+      setTriggerRefresh(false);
+    }
+  }, [triggerRefresh]);
 
-      // Only start dragging if the mouse is down
+  const bind = useDrag(
+    ({
+      movement: [, my],
+      memo = viewportRef.current?.scrollTop || 0,
+      event,
+      dragging,
+      last,
+      velocity,
+    }) => {
       if (!isMouseDown || !event.target || (event.target as HTMLElement).closest('.slide-tray')) {
-        console.log('Drag ignored due to slide tray, invalid target, or mouse not being down.');
         return memo;
       }
 
-      if (!dragStarted && my !== 0) setDragStarted(true); // Only set dragStarted if there's movement
+      if (!dragStarted && my !== 0) setDragStarted(true);
 
       if (viewportRef.current) {
         const viewport = viewportRef.current;
         const atTop = viewport.scrollTop <= 0;
         const atBottom = viewport.scrollTop + viewport.clientHeight >= viewport.scrollHeight;
 
+        const maxBottomOverscrollDistance = viewport.clientHeight * BOTTOM_OVERSCROLL_PERCENTAGE;
+        const limitedOverscroll = atTop
+          ? Math.min(my / 2, TOP_OVERSCROLL_LIMIT)
+          : Math.max(my / 2, -maxBottomOverscrollDistance);
+
         if (dragging) {
           if ((atTop && my > 0) || (atBottom && my < 0)) {
-            api.start({ y: my / 2 });
+            const overscrollPercentage = (limitedOverscroll / TOP_OVERSCROLL_LIMIT) * 100;
+            console.log(
+              `Overscroll Value: ${limitedOverscroll}px, Percentage: ${overscrollPercentage.toFixed(2)}%, Threshold: ${OVERSCROLL_ACTIVATION_THRESHOLD}`,
+            );
+
+            api.start({
+              y: limitedOverscroll,
+              loaderOpacity: atTop ? limitedOverscroll / TOP_OVERSCROLL_LIMIT : 0,
+            });
           } else {
             viewport.scrollTop = memo - my;
-            api.start({ y: 0 });
+            api.start({ y: 0, loaderOpacity: 0 });
           }
-        } else if (last) {
-          if (atTop && my > 0) {
-            api.start({ y: 0, immediate: false });
-          } else if (atBottom && my < 0) {
-            api.start({ y: 0, immediate: false });
+        } else if (last && !isRefreshing) {
+          const velocityMagnitude = Math.sqrt(velocity[0] ** 2 + velocity[1] ** 2);
+          // Only trigger refresh if velocity is below threshold
+          if (
+            atTop &&
+            limitedOverscroll > OVERSCROLL_ACTIVATION_THRESHOLD &&
+            velocityMagnitude < MAX_REFRESH_VELOCITY
+          ) {
+            api.start({ y: TOP_OVERSCROLL_LIMIT, loaderOpacity: 1 });
+            setTriggerRefresh(true);
+          } else {
+            api.start({ y: 0, loaderOpacity: 0 });
           }
-          setDragStarted(false); // Reset dragStarted on drag end
-        }
-
-        if (atTop && my > 100 && !isRefreshing) {
-          console.log('Triggering refresh');
-          handleRefresh();
+          setDragStarted(false);
         }
       }
 
@@ -83,25 +114,22 @@ export const TileScroller: React.FC<TileScrollerProps> = ({
     },
   );
 
-  // Handle mouse down/up to track if mouse is actively pressed
-  const handleMouseDown = () => {
-    setIsMouseDown(true);
-    console.log('Mouse down - ready to start drag.');
-  };
-
+  const handleMouseDown = () => setIsMouseDown(true);
   const handleMouseUp = () => {
     setIsMouseDown(false);
     setDragStarted(false);
-    api.start({ y: 0 });
-    console.log('Mouse up - reset drag state.');
+    if (!isRefreshing) {
+      api.start({ y: 0, loaderOpacity: 0 });
+    }
   };
 
   useEffect(() => {
     const resetDrag = () => {
-      console.log('Global reset drag - Mouse or pointer left window.');
       setIsMouseDown(false);
       setDragStarted(false);
-      api.start({ y: 0 });
+      if (!isRefreshing) {
+        api.start({ y: 0, loaderOpacity: 0 });
+      }
     };
 
     window.addEventListener('mouseup', resetDrag);
@@ -113,15 +141,13 @@ export const TileScroller: React.FC<TileScrollerProps> = ({
       window.removeEventListener('mouseleave', resetDrag);
       window.removeEventListener('pointercancel', resetDrag);
     };
-  }, [api]);
+  }, [api, isRefreshing]);
 
-  // TODO: enable swipe to refresh
-  // animate drag into position of loader above scroll area
-  // re-query for data
-  // re-populate data and remove loader
-  // add max overscroll to bottom?
+  // TODO: on refresh, re-query for data
+  // TODO: on refresh completion, re-populate data and remove loader
   return (
     <ScrollArea
+      // TODO: move select-none to top level, add select option to specific areas of app that allow it (inputs)
       className="select-none flex-grow w-full overflow-y-auto border border-neutral-3 rounded-md"
       type="always"
       scrollbarProps={{}}
@@ -130,15 +156,23 @@ export const TileScroller: React.FC<TileScrollerProps> = ({
       onMouseUp={handleMouseUp}
     >
       <animated.div
-        className="pr-3"
+        className="relative pr-3"
         style={{
           transform: y.to(v => `translateY(${v}px)`),
         }}
         {...bind()}
       >
-        {isRefreshing ? (
-          <div className="text-center py-4">Refreshing...</div>
-        ) : activeIndex === 0 ? (
+        <animated.div
+          className="absolute top-[-52px] left-0 right-0 flex justify-center items-center h-12"
+          style={{
+            opacity: loaderOpacity,
+            transform: y.to(v => `translateY(${Math.max(v - 52, -52)}px)`),
+          }}
+        >
+          <Loader isSpinning={isRefreshing} />
+        </animated.div>
+
+        {activeIndex === 0 ? (
           <AssetTiles
             isSelectable={isSelectable}
             onClick={onSelectAsset}
