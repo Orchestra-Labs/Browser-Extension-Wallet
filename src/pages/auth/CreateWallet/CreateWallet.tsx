985ebf3a63bca63855fb600e20a76b65b0f45f34
dblackstone1 <dblackstone@orchestralabs.org>
2024-10-29 12:06:33 -0400

    chore: move tasks around on todos

--- src/pages/auth/CreateWallet/CreateWallet.tsx
+++ src/pages/auth/CreateWallet/CreateWallet.tsx
@@ -108,6 +108,7 @@ export const CreateWallet = () => {
   // TODO: Add QR code intake methods (camera, file selection/drag and drop option)
   // TODO: Add Maestro-only QR code that also shows preferred receive asset (requires preferred receive asset)
 
+  // TODO: pull unbonding days dynamically from the validator
   // TODO: add fee display and updates for stake, unstake, and claim
 
   // TODO: make "clear" and "max" button placement and appearance more uniform (send and unstake sections)
@@ -126,10 +127,22 @@ export const CreateWallet = () => {
   // TODO: test path and create error for no wallet exists and user attempts login
   // TODO: handle error printout for create/import wallet (in place of subtitle on verify screen?)
 
+  /* Interchain-compatibility TODOs (mobile version before this) */
+  // TODO: add button to "add chain" at bottom of Holdings list
+  // TODO: abstract wallet prefix and mnemonic decryption
+  // TODO: add link to github repo for registry
+  // TODO: abstract IBC needs
+  // TODO: ensure pipeline to other registries is functional.  change here auto-PRs there
+  // TODO: integrate skip protocol or automated use of exchange to allow swapping between chains
+  // TODO: modify claim, restake, and unstake functions to withdraw from all unique delegator addresses
+  // TODO: enable search function for wallet asset list by chain (on top of current functionality)
+  // TODO: enable search function to validator list by chain (on top of current functionality)
+  // TODO: add show/hide function to validator list (by chain)
+
   /* Wallet UI TODOs */
   // TODO: create add wallet screen to allow management of multiple accounts
+  // TODO: add save wallet screen for saving preferred received assets per wallet and wallet name/identifier (for those user sends to)
   // TODO: add show/hide function to wallet asset list (select which assets to show.  add searchability to this)
-  // TODO: add show/hide function to validator list (by activity, by chain)
   // TODO: security tab enables/disables need to confirm transactions/re-entry of password on transactions (3 levels of security)
   // TODO: update transactions history button (need endpoint for this.  disable until ready)
   // TODO: show donut chart with assets rather than singular value (requires connections to exchanges)
@@ -140,19 +153,7 @@ export const CreateWallet = () => {
   // TODO: Add NFT page
   // TODO: Add page view selection in options
 
-  /* Interchain-compatibility TODOs */
-  // TODO: add button to "add chain" at bottom of Holdings list
-  // TODO: abstract wallet prefix and mnemonic decryption
-  // TODO: add link to github repo for registry
-  // TODO: abstract IBC needs
-  // TODO: ensure pipeline to other registries is functional.  change here auto-PRs there
-  // TODO: integrate skip protocol or automated use of exchange to allow swapping between chains
-  // TODO: modify claim, restake, and unstake functions to withdraw from all unique delegator addresses
-  // TODO: enable search function for wallet asset list by chain (on top of current functionality)
-  // TODO: enable search function to validator list by chain (on top of current functionality)
-
   /* Less Critical Wallet TODOs */
-  // TODO: add save wallet screen for saving preferred received assets per wallet and wallet name/identifier (for those user sends to)
   // TODO: add data transfer method for extension to wallet or back (including account data)
   // TODO: ensure refresh only queries once (currently 4 times per pull)
 
@@ -164,8 +165,8 @@ export const CreateWallet = () => {
   // TODO: add password complexity bar on entry
   // TODO: add toggle option for single-click transactions and another for force accepting/declining transactions
   // TODO: add toggle option for ultra-secure mode, using password auth for on every transaction like with hardware wallets.  in that mode, no session storage is used.
-  // TODO: loader on tilerscroller swipe-to-refresh coming in late and tries to catch up.  make more uniform.
-  // TODO: loader on tilerscroller swipe-to-refresh should spin in line with its percent pulled
+  // TODO: loader on tilescroller swipe-to-refresh coming in late and tries to catch up.  make more uniform.
+  // TODO: loader on tilescroller swipe-to-refresh should spin in line with its percent pulled
   // TODO: add option screen filter for validator list (by status.  default to not showing jailed validators.  check fetchAllValidators function and validatorsAtom (currently filters here))
 
   /* Nice to have Interchain-compatibility TODOs */
