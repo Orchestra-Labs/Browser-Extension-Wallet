8971d3f71d4c3e88dc55d930265df7f000022ca5
dblackstone1 <dblackstone@orchestralabs.org>
2024-10-30 20:41:22 -0400

    chore: amend todo, add specifics

--- src/helpers/claimRewards.ts
+++ src/helpers/claimRewards.ts
@@ -124,7 +124,7 @@ export const claimRewards = async (
   }
 };
 
-// TODO: fails occasionally.  find out why and fix.  needs timeout?
+// TODO: fails occasionally on restake.  find out why and fix.  needs timeout?
 // Function to claim rewards and restake for one or multiple validators
 export const claimAndRestake = async (
   delegations: DelegationResponse | DelegationResponse[],
