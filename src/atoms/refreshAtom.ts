dc797ec00e531aae631d65c04bb2a23d68eee2de
dblackstone1 <dblackstone@orchestralabs.org>
2024-11-04 18:13:19 -0500

    chore: remove unnecessary atom

--- src/atoms/refreshAtom.ts
+++ src/atoms/refreshAtom.ts
@@ -2,7 +2,3 @@ import { atom } from 'jotai';
 
 export const isFetchingWalletDataAtom = atom(false);
 export const isFetchingValidatorDataAtom = atom(false);
-export const isFetchingDataAtom = atom(get => ({
-  wallet: get(isFetchingWalletDataAtom),
-  validator: get(isFetchingValidatorDataAtom),
-}));
