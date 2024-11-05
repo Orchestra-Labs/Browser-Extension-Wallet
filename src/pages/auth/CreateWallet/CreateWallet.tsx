340aa10a5e2a8a78479bbbe7841bafddc38fb24e
dblackstone1 <dblackstone@orchestralabs.org>
2024-11-05 14:11:34 -0500

    chore: re-assign loader work to david in todos

--- src/pages/auth/CreateWallet/CreateWallet.tsx
+++ src/pages/auth/CreateWallet/CreateWallet.tsx
@@ -105,11 +105,11 @@ export const CreateWallet = () => {
   /* David Current TODOs */
   // TODO: enable transaction fees for wallet transactions (bundle a transaction for revenue. based on transaction fees (0.5x basic transaction fee)?)
   // TODO: fix error with excessive calls to refresh
+  // TODO: use loader on balance card for first load
   /* ******************************************************************************************* */
 
   /* Current TODOs */
   // TODO: versioning: minimum, recommended, and current versions.  recommended update for versions lower than recommended, link to update, no login for versions below minimum
-  // TODO: use loader on balance card for first load
 
   /* Interchain-compatibility TODOs (mobile version before this) */
   // TODO: abstract wallet prefix and mnemonic decryption
