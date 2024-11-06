c5dd0ccc61d2a41229d2faf5cdb55169363b62db
dblackstone1 <dblackstone@orchestralabs.org>
2024-11-05 20:14:34 -0500

    feat: fix visuals to not move on change to not loading

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -14,9 +14,9 @@ export const Loader = ({
 }: LoaderProps) => (
   <div
     className={cn(
-      `${showBackground ? 'bg-background-dark-grey' : ''} ${scaledHeight ? 'mt-2' : ''} w-full h-full flex items-center justify-center`,
+      `${showBackground ? 'bg-background-dark-grey' : ''} ${scaledHeight ? 'mt-2 mb-[0.15rem]' : ''} w-full h-full flex items-center justify-center`,
     )}
   >
-    <Spinner className={cn(`w-12 h-12 fill-blue ${isSpinning ? `animate-spin` : ''}`)} />
+    <Spinner className={cn(`w-11 h-11 fill-blue ${isSpinning ? `animate-spin` : ''}`)} />
   </div>
 );
